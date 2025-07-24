60d6ff37265c9449e30d14ce122074f4c36b7542
[nayankumar7] <[nayankumarvishwakarma1@gmail.com]>
2025-07-24 13:14:53 +0530

    add new learning project is learning001

---
+++ learning001/learning001/src/main/java/com/learning/learning001/Learning001Application.java
@@ -0,0 +1,13 @@
+package com.learning.learning001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning001Application.class, args);
+	}
+
+}
---
+++ learning001/learning001/src/test/java/com/learning/learning001/Learning001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.learning001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learning001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
